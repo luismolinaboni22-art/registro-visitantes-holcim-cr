0a8be962ba623964fc2c3affe0ee4896b0e9edcc
luismolinaboni22-art <luismolinaboni22@gmail.com>
2025-12-03 05:17:38 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Base de datos temporal en memoria (luego puedes pasar esto a SQLite o MySQL)
+# Base de datos temporal en memoria
 visitantes = []
 
 @app.route('/')
@@ -12,22 +12,14 @@ def index():
 
 @app.route('/agregar', methods=['POST'])
 def agregar():
-    nombre = request.form['nombre']
-    cedula = request.form['cedula']
-    empresa = request.form['empresa']
-    persona_visita = request.form['persona_visita']
-    motivo = request.form['motivo']
-    placa = request.form['placa']
-    hora_ingreso = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
     visitante = {
-        "nombre": nombre,
-        "cedula": cedula,
-        "empresa": empresa,
-        "persona_visita": persona_visita,
-        "motivo": motivo,
-        "placa": placa,
-        "hora_ingreso": hora_ingreso,
+        "nombre": request.form['nombre'],
+        "cedula": request.form['cedula'],
+        "empresa": request.form['empresa'],
+        "persona_visita": request.form['persona_visita'],
+        "motivo": request.form['motivo'],
+        "placa": request.form['placa'],
+        "hora_ingreso": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hora_salida": ""
     }
 
